dcda85291a927878f7ec198873798c110f72efcb
AndreyMj <itsochi90@gmail.com>
2022-08-08 16:23:16 +0300

    Initial commit

---
+++ src/test/java/com/movsesian/IocDiFinishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movsesian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDiFinishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
